cc0e5aa34b1e007f20a4abcd1b44cc13fb2aa630
onsefaitchier <julien.gaspar00@gmail.com>
2016-04-03 15:54:15 +0200

    essai
    juste un essai

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+
+<form method="post" action="traitement.php">
+    <p>
+        <label for="pseudo">Votre pseudo :</label>
+        <input type="text" name="pseudo" id="pseudo" size="30" maxlength="10" />
+
+       <br />
+       <label for="pass">Votre mot de passe :</label>
+       <input type="password" name="pass" id="pass" />
+    </p>
+    <p>
+    	<label for="nom">Votre Nom :</label>
+    	<input type="text" name="nom" id="nom" size="30" maxlength="10" />
+    </p>
+    <p>
+    	<label for="prenom">Votre Prénom:</label>
+    	<input type="text" name="prénom" id="prenom" size="30" maxlength="10" />
+    </p>
+    <p>
+    	<input type="submit" value="OK">
+    </p>
+   
+</form>
+
+</html>
